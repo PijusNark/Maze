302accf2b3df451ea632b8f80d585a0156647029
PijusNark <113291566+PijusNark@users.noreply.github.com>
2024-10-29 18:43:27 +0300

    Lelevelcompliation.cs
    Garsas ir nuotrauka

--- Assets/Scripts/Lelevelcompliation.cs
+++ Assets/Scripts/Lelevelcompliation.cs
@@ -3,10 +3,10 @@ using UnityEngine.UI; // Required for UI elements
 
 public class LevelCompletion : MonoBehaviour
 {
-    public GameObject completionImage; // Assign the UI Image object
-    public AudioSource completionSound; // Assign the Audio Source object
+    public GameObject completionImage; 
+    public AudioSource completionSound; 
 
-    // Call this method when the player completes the level
+    
     public void OnLevelComplete()
     {
         // Play the completion sound
